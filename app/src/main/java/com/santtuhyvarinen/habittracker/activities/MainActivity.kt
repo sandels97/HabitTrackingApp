fafef6d36671964eb6ca8da70654975b162e4957
Santtu Hyvärinen <santtu.hyvarine@gmail.com>
2020-12-13 14:11:55 +0200

    Added NavController and BottomNavigationBar

--- app/src/main/java/com/santtuhyvarinen/habittracker/activities/MainActivity.kt
+++ app/src/main/java/com/santtuhyvarinen/habittracker/activities/MainActivity.kt
@@ -2,15 +2,19 @@ package com.santtuhyvarinen.habittracker.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.santtuhyvarinen.habittracker.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun setFragment() {
-        supportFragmentManager.beginTransaction().commit()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigation.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/santtuhyvarinen/habittracker/fragments/HabitsFragment.kt
@@ -0,0 +1,20 @@
+package com.santtuhyvarinen.habittracker.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.santtuhyvarinen.habittracker.R
+
+class HabitsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_habits, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
