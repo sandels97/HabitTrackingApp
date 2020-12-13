2d432886100ac10f065c91a881522826ea44d5ba
Santtu Hyvärinen <santtu.hyvarine@gmail.com>
2020-12-13 14:56:46 +0200

    Added Toolbar

--- app/src/main/java/com/santtuhyvarinen/habittracker/activities/MainActivity.kt
+++ app/src/main/java/com/santtuhyvarinen/habittracker/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.santtuhyvarinen.habittracker.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.santtuhyvarinen.habittracker.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
+        //Set up bottom navigation bar and toolbar with NavController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
         bottomNavigation.setupWithNavController(navController)
     }
 }
