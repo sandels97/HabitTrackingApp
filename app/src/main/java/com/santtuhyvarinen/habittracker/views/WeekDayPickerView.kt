da7d386901df4a91141701c1bbc705e702191875
Santtu Hyvärinen <santtu.hyvarine@gmail.com>
2021-01-20 10:20:00 +0200

    Fixed a bug that caused WeekDayPickerView to not work

--- app/src/main/java/com/santtuhyvarinen/habittracker/views/WeekDayPickerView.kt
+++ app/src/main/java/com/santtuhyvarinen/habittracker/views/WeekDayPickerView.kt
@@ -2,6 +2,7 @@ package com.santtuhyvarinen.habittracker.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.FrameLayout
@@ -21,8 +22,6 @@ class WeekDayPickerView(context: Context, attributeSet: AttributeSet) : FrameLay
     private lateinit var buttons : Array<Button>
 
     init {
-        inflate(context, R.layout.layout_weekday_picker, this)
-
         initializeWeekDayButtons()
     }
 
